d633429a802677711d714b5b993e2aa20f4fc936
Sergey Lapin <sv0lapin@gmail.com>
2016-05-20 13:58:26 +0300

    react preset for babel

--- ui/js/app.js
+++ ui/js/app.js
@@ -1,2 +1,19 @@
-import main from './main/main';
-main();
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Hello!</div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
